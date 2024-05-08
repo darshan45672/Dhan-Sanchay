325b00c1eb96a87b4339b06d0b0cc1e1c336347d
darshan45672 <drshnbhandary@gmail.com>
2024-05-08 10:43:10 +0530

    implemented dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalance from '@/components/TotalBalance';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'John', lastName: 'Doe'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox
+          type='greeting'
+          title='Welcome'
+          user = {loggedIn?.firstName + ' ' + loggedIn?.lastName || 'Guest'}
+          subtext = "Acess and manage your account, view your balance and more."
+          />
+        </header>
+        <TotalBalance 
+        accounts ={[]}
+        totalBank = {1}
+        totalCurrentBalance = {1500.01}
+        />
+      </div>
+    </section>
   )
 }
 
