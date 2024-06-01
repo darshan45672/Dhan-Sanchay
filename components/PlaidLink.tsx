05f86538aa323aac0fa678905dd270c2d2d2aeee
darshan45672 <drshnbhandary@gmail.com>
2024-06-01 23:40:44 +0530

    fixed minor typo error

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -26,7 +26,7 @@ const PlaidLink = ({user, variant}:PlaidLinkProps) => {
             user,
         })
         router.push('/');
-    },[user]);
+    },[router, user]);
 
     const config: PlaidLinkOptions = {
         token, onSuccess,
